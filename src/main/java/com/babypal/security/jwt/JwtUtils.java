3df0aea0ed1a856426848d382dd445e170431e8b
Ng Zheng Jie <zhengjie.zjie@gmail.com>
2025-09-12 22:05:47 +0800

    chore: Disable unused import

--- src/main/java/com/babypal/security/jwt/JwtUtils.java
+++ src/main/java/com/babypal/security/jwt/JwtUtils.java
@@ -7,7 +7,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.userdetails.UserDetails;
+// import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import com.babypal.security.services.UserDetailsImpl;
